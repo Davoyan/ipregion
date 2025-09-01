643d77350613a9ab48f4c8d40cf10d9cc080eab0
Davoyan <chikiti@ya.ru>
2025-09-01 16:01:47 +0300

    Update ipregion.sh

--- ipregion.sh
+++ ipregion.sh
@@ -1684,7 +1684,18 @@ lookup_google() {
 	else
 		curl_ip_flag="-4"
 	fi
-    country=$(timeout "$CURL_TIMEOUT" curl $curl_ip_flag -sL 'https://play.google.com/'   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'   -H 'accept-language: en-US;q=0.9'   -H 'priority: u=0, i'   -H 'sec-ch-ua: "Chromium";v="131", "Not_A Brand";v="24", "Google Chrome";v="131"'   -H 'sec-ch-ua-mobile: ?0'   -H 'sec-ch-ua-platform: "Windows"'   -H 'sec-fetch-dest: document'   -H 'sec-fetch-mode: navigate'   -H 'sec-fetch-site: none'   -H 'sec-fetch-user: ?1'   -H 'upgrade-insecure-requests: 1' -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36' | grep -oP '<div class="yVZQTb">\K[^<(]+')
+	
+	curl_args=()
+	
+	if [[ -n "$PROXY_ADDR" ]]; then
+	  curl_args+=(--proxy "socks5://$PROXY_ADDR")
+	fi
+
+	if [[ -n "$INTERFACE_NAME" ]]; then
+	  curl_args+=(--interface "$INTERFACE_NAME")
+	fi
+	
+    country=$(timeout "$CURL_TIMEOUT" curl $curl_ip_flag -sL "${curl_args[@]}" 'https://play.google.com/'   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7'   -H 'accept-language: en-US;q=0.9'   -H 'priority: u=0, i'   -H 'sec-ch-ua: "Chromium";v="131", "Not_A Brand";v="24", "Google Chrome";v="131"'   -H 'sec-ch-ua-mobile: ?0'   -H 'sec-ch-ua-platform: "Windows"'   -H 'sec-fetch-dest: document'   -H 'sec-fetch-mode: navigate'   -H 'sec-fetch-site: none'   -H 'sec-fetch-user: ?1'   -H 'upgrade-insecure-requests: 1' -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36' | grep -oP '<div class="yVZQTb">\K[^<(]+')
 
 	country=$(echo "$country" | xargs)  # убираем пробелы
 	if [[ -n "$country" ]]; then
